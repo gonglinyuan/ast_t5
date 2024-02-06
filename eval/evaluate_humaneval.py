9aa23e2a730b6bd2bc8957952559e22b0e47c1a9
Linyuan Gong <gonglinyuan@hotmail.com>
2024-02-06 15:46:23 -0500

    Fix

--- eval/evaluate_humaneval.py
+++ eval/evaluate_humaneval.py
@@ -1,7 +1,79 @@
 import argparse
 import json
+from collections import Counter, defaultdict
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import List
 
-from human_eval.evaluation import evaluate_functional_correctness
+import numpy as np
+import tqdm
+from human_eval.data import HUMAN_EVAL, maybe_stream_jsonl, read_problems
+from human_eval.evaluation import estimate_pass_at_k
+from human_eval.execution import check_correctness
+
+
+def evaluate_functional_correctness(
+    sample_file: str,
+    k: List[int] = [1, 10, 100],
+    n_workers: int = 4,
+    timeout: float = 3.0,
+    problem_file: str = HUMAN_EVAL,
+):
+    """
+    Evaluates the functional correctness of generated samples, and writes
+    results to f"{sample_file}_results.jsonl.gz"
+    """
+
+    problems = read_problems(problem_file)
+
+    # Check the generated samples against test suites.
+    with ThreadPoolExecutor(max_workers=n_workers) as executor:
+
+        futures = []
+        completion_id = Counter()
+        n_samples = 0
+        results = defaultdict(list)
+
+        print("Reading samples...")
+        for sample in tqdm.tqdm(maybe_stream_jsonl(sample_file)):
+            task_id = sample["task_id"]
+            completion = sample["completion"]
+            args = (problems[task_id], completion, timeout, completion_id[task_id])
+            future = executor.submit(check_correctness, *args)
+            futures.append(future)
+            completion_id[task_id] += 1
+            n_samples += 1
+
+        assert len(completion_id) == len(problems), "Some problems are not attempted."
+
+        print("Running test suites...")
+        for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
+            result = future.result()
+            results[result["task_id"]].append((result["completion_id"], result))
+
+    # Calculate pass@k.
+    total, correct = [], []
+    for result in results.values():
+        result.sort()
+        passed = [r[1]["passed"] for r in result]
+        total.append(len(passed))
+        correct.append(sum(passed))
+    total = np.array(total)
+    correct = np.array(correct)
+
+    ks = k
+    pass_at_k = {f"pass@{k}": estimate_pass_at_k(total, correct, k).mean()
+                 for k in ks if (total >= k).all()}
+
+    # Finally, save the results in one file:
+    def combine_results():
+        for sample in maybe_stream_jsonl(sample_file):
+            task_id = sample["task_id"]
+            result = results[task_id].pop(0)
+            sample["result"] = result[1]["result"]
+            sample["passed"] = result[1]["passed"]
+            yield sample
+
+    return pass_at_k, list(combine_results())
 
 
 def main():
